e4c8c73ce430e653017175873e49963e4aac036d
Wendy <152674682+vanilla-icecream23@users.noreply.github.com>
2025-05-08 12:40:39 +0700

    Add files via upload

---
+++ cell04/ex03/float.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+x=float(input("Give me a number : "))
+
+if x.is_integer():
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+x=str(input(""))
+y=x.swapcase()
+print(y)
