11941a222441c11b5894fdf6c7f2d2f11ebdf0a9
Wendy <152674682+vanilla-icecream23@users.noreply.github.com>
2025-05-09 12:55:29 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+origin = [2, 8, 9, 48, 22, -12, 2]
+new = []
+for x in origin:
+    new.append(x+2)
+print(origin)
+print(new)
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+origin = [2, 8, 9, 48, 22, -12, 2]
+new = [(x+2) for x in origin if x > 5]
+print (origin)
+print (new)
